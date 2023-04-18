db6bfe2267373e0edaf3e48e675a0cb99887aa64
aryan <90237450+aaryandeveloper@users.noreply.github.com>
2023-04-18 12:34:14 +0530

    Update Trellocurl.php

--- .github/workflows/Trellocurl.php
+++ .github/workflows/Trellocurl.php
@@ -1,36 +1,23 @@
-# This is a basic workflow to help you get started with Actions
+<?php
 
-name: CI
+$curl = curl_init();
 
-# Controls when the workflow will run
-on:
-  # Triggers the workflow on push or pull request events but only for the "main" branch
-  push:
-    branches: [ "main" ]
-  pull_request:
-    branches: [ "main" ]
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.trello.com/1/cards?name=trello&desc=test&idList=your_list_id&urlSource=trello.com&&key=your_api_key&token=your_api_token',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_HTTPHEADER => array(
+    'Accept: application/json',
+    'Cookie: dsc=2544a16b2ae3ab182ca0cf5320894b183a7fc25ae6017d0248088af0ca972128'
+  ),
+));
 
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
+$response = curl_exec($curl);
 
-# A workflow run is made up of one or more jobs that can run sequentially or in parallel
-jobs:
-  # This workflow contains a single job called "build"
-  build:
-    # The type of runner that the job will run on
-    runs-on: ubuntu-latest
-
-    # Steps represent a sequence of tasks that will be executed as part of the job
-    steps:
-      # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
-      - uses: actions/checkout@v3
-
-      # Runs a single command using the runners shell
-      - name: Run a one-line script
-        run: echo Hello, world!
-
-      # Runs a set of commands using the runners shell
-      - name: Run a multi-line script
-        run: |
-          echo Add other actions to build,
-          echo test, and deploy your project.
+curl_close($curl);
+echo $response;
